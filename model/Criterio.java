c22cfb394a2756cf9c3706a58072d5a26e70f979
felipe-medeiros <felipe.medeiros.alves@gmail.com>
2017-10-10 20:24:44 -0300

    versao final

--- model/Criterio.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.List;
-
-public interface Criterio {
-    public Ficha puxa (List<Ficha> listnre,List<Ficha> listpre,List<Ficha> listnfe,List<Ficha> listpfe);
-    public String getTipo();
-}
